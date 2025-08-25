ec35611404290bf5c575f8a3e0bd1c2c6e55860b
Sadaf-Aarfeen <147734957+Sadaf-Aarfeen@users.noreply.github.com>
2025-08-25 11:39:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function loadPlaylist(folderName) {
     sList.innerHTML = "";
 
     //fetching songs from a particular folder
-let a = await fetch(`TuneCraft---Made-For-Listeners/songs/${folderName}/songs.json`);
+let a = await fetch(`/TuneCraft---Made-For-Listeners/songs/${folderName}/songs.json`);
 
     let res = await a.text();
 
@@ -232,6 +232,7 @@ themeButton.addEventListener("click", () => {
     document.body.classList.toggle("light-theme");
 
 })
+
 
 
 
